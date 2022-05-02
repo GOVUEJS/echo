a17e2061b828dadd4283a6864335ae56b7d51481
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-05-02 16:59:17 +0900

    로그

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -8,6 +9,11 @@ import (
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
