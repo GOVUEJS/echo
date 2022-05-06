e56b036869910bce403b66ea3ab1da96c95d9c98
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-05-06 17:19:51 +0900

    Articles.DeletedAt 설정

--- model/model.go
+++ model/model.go
@@ -1,14 +1,15 @@
 package model
 
 import (
+	"gorm.io/gorm"
 	"time"
 )
 
 type Article struct {
-	Id        int       `gorm:"primarykey" json:"id"`
-	Title     string    `json:"title" json:"title"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"date"`
-	DeletedAt time.Time `gorm:"index" json:"-"`
+	Id        int            `gorm:"primaryKey" json:"id"`
+	Title     string         `json:"title" json:"title"`
+	Content   string         `json:"content"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"date"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
