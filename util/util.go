994739273584161ed7199ba3d37b888ded364e81
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-05-19 01:59:22 +0900

    API 응답용 Response 함수 생성

--- util/util.go
+++ util/util.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func Response(c echo.Context, status int, message *string, data interface{}) error {
+func Response(c echo.Context, status int, message string, data interface{}) error {
 	response := make(map[string]interface{})
 
 	m2, _ := json.Marshal(data)
@@ -15,7 +15,10 @@ func Response(c echo.Context, status int, message *string, data interface{}) err
 		return c.JSON(http.StatusInternalServerError, "Internal Error")
 	}
 
-	apiResponse := model.ApiResponse{Message: message}
+	apiResponse := model.ApiResponse{}
+	if message != "" {
+		apiResponse.Message = &message
+	}
 	m1, _ := json.Marshal(apiResponse)
 	if err := json.Unmarshal(m1, &response); err != nil {
 		return c.JSON(http.StatusInternalServerError, "Internal Error")
