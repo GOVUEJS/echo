02aef926a5a72333ba4e3502ae84ca1718d0a413
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-05-21 02:20:44 +0900

    echo 로그 내용 추가

--- server/server.go
+++ server/server.go
@@ -14,8 +14,24 @@ func init() {
 	e = echo.New()
 
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "method=${method}, uri=${uri}, status=${status}\n ",
+		Format: `{` +
+			`"time":"${time_rfc3339_nano}"` +
+			`"id":"${id}","remote_ip":"${remote_ip}",` +
+			`"host":"${host}",` +
+			`"method":"${method}",` +
+			`"uri":"${uri}",` +
+			`"form":"${form}",` +
+			`"user_agent":"${user_agent}",` +
+			`"status":${status},` +
+			`"error":"${error}",` +
+			`"latency":${latency},` +
+			`"latency_human":"${latency_human}",` +
+			`"bytes_in":${bytes_in},` +
+			`"bytes_out":${bytes_out}` +
+			"}\n",
+		CustomTimeFormat: "2006-01-02 15:04:05.00000",
 	}))
+
 	e.Use(middleware.CORS())
 }
 
