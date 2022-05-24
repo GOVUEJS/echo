fa3100f4fc851d979470289e9a9c8e4240ac0fc8
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-05-24 23:31:14 +0900

    config-example.toml 추가

--- config/config.go
+++ config/config.go
@@ -5,11 +5,11 @@ import (
 )
 
 type config struct {
-	Phase string
-	Jwt   jwt
-	Echo  echoConfig
-	Rdb   rdbConfig
-	Redis redisConfig
+	Phase string      `toml:"phase"`
+	Jwt   jwt         `toml:"jwt"`
+	Echo  echoConfig  `toml:"echo"`
+	Rdb   rdbConfig   `toml:"rdb"`
+	Redis redisConfig `toml:"redis"`
 }
 
 type jwt struct {
@@ -18,22 +18,22 @@ type jwt struct {
 }
 
 type echoConfig struct {
-	Port uint
+	Port uint `toml:"port"`
 }
 
 type rdbConfig struct {
-	Ip            string
-	Port          uint
-	User          string
-	Password      string
-	DbName        string
-	AutoMigration bool
+	Ip            string `toml:"ip"`
+	Port          uint   `toml:"port"`
+	User          string `toml:"user"`
+	Password      string `toml:"password"`
+	DbName        string `toml:"dbName"`
+	AutoMigration bool   `toml:"auto_migration"`
 }
 
 type redisConfig struct {
-	Ip       string
-	Port     uint
-	Password string
+	Ip       string `toml:"ip"`
+	Port     uint   `toml:"port"`
+	Password string `toml:"password"`
 }
 
 var (
